66b1c5bb15b608949c7047521cc8343cacc1152b
Merlin Nguyen <jonhenry13241@gmail.com>
2021-04-15 01:03:08 +0700

    add example secret.h

---
+++ secret.h.d
@@ -0,0 +1,7 @@
+
+#define SPOTIFY_MARKET "US"
+#define SPOTIFY_REFRESH_TOKEN "spotify refresh token"
+#define _SSID "ssid"
+#define _SSID_PWD "ssid password"
+#define SPOTIFY_CLIENT_ID "clientId"
+#define SPOTIFY_CLIENT_SECRET "clientSecret"
